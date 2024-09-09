5ccf3f6499b8e27511e4123131aed9370b487a51
Nile Roth <102401663+nile-roth@users.noreply.github.com>
2024-09-09 09:18:21 -0700

    Delete index.js
    redo structure

--- index.js
+++
@@ -1,6 +0,0 @@
-//dependencies
-const express = require('express');            //express.js for backend framework
-const sqlite3 = require('sqlite3').verbose();  //sqlite for local database
-const bcrypt = require('bcryptjs');            //for password hashing 
-const jwt = require('jsonwebtoken');           //for passing task data to/from server
-const cors = require('cors');                  //allows frontend and backend to communicate on different ports
