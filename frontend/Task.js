51e4ceb7b50c783122a216189b98485a973d235d
Nile Roth <102401663+nile-roth@users.noreply.github.com>
2024-09-18 21:17:09 -0700

    Update Task.js

--- frontend/Task.js
+++ frontend/Task.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Task({task, handleDelete}) {
     return(
-        <div>
+        <div class="task">
+            {task.taskDate}
+            <br></br>
+            {task.taskTime}
+            <br></br>
             {task.taskTitle}
             <button onClick={() => handleDelete(task.id)}>X</button>
         </div>
